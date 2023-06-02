0baf1f378992b34fa4334779a4a23366a86bb517
EmanuelPT <130150959+EmanuelPT@users.noreply.github.com>
2023-06-02 10:48:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZW1hbnVlbHB0IiwiYSI6ImNsaWViaXprczA3djkzanA2cTJncDk5am0ifQ.2Te8YgHjlwTxnVg04wDsbw';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
